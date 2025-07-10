1c1c41be9cc53790dfd242355a88b030a63028ce
Prashik Patil <70844686+hotshott22@users.noreply.github.com>
2025-07-10 09:10:46 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.69%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+
+        string res;
+        int bal=0;
+
+        for(char c:s){
+            if(c=='(' && bal++>0) res+=c;
+            if(c==')' && bal-->1) res+=c;
+        }
+        return res;
+        
+    }
+};
