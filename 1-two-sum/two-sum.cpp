75bb3ac726f1c8503497b21ff1e87215e033d386
Prashik Patil <70844686+hotshott22@users.noreply.github.com>
2025-08-27 10:57:08 +0530

    Time: 3 ms (70.55%) | Memory: 14.8 MB (36.15%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    // vector<int> twoSum(vector<int>& nums, int target) {
-        
-    // }
-
     vector<int> twoSum(vector<int>& nums, int target) {
-    // Store the indices of the two elements that add up to the target
-    vector<int> indices;
 
-    // Iterate through the array of integers
-    for (int i = 0; i < nums.size(); i++) {
-        // Use a nested loop to check if any other element adds up to the target
-        for (int j = i + 1; j < nums.size(); j++) {
-            // If a pair is found that adds up to the target, store their indices and return them
-            if (nums[i] + nums[j] == target) {
-                indices.push_back(i);
-                indices.push_back(j);
-                return indices;
-            }
+        unordered_map<int,int>mp;
+
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            int c=target-nums[i];
+            if(mp.count(c)) return {mp[c],i};
+            mp[nums[i]]=i;
         }
+        
+        return {};
     }
-
-    return indices;
-}
 };
