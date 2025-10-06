ccdb5bec02ab7afe385f9a65b759e7d39d6d7373
Prashik Patil <70844686+hotshott22@users.noreply.github.com>
2025-10-06 14:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (79.77%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-
-    int am(vector<int>&nums , int goal)
-    {
-         int l=0,r=0,ans=0,count=0;
-
-        int n=nums.size();
-
-        if(goal<0)
-            return 0; 
-
-        for(r=0;r<nums.size();r++){
-            count+=nums[r];
-
-            while(count>goal){
-                count-=nums[l];
-                l++;
-            }
-            ans+=(r-l+1);
-        }    
-        return ans;
-    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-
-          return am(nums,goal)-am(nums,goal-1);
+        return atMost(nums, goal) - atMost(nums, goal-1);
+    }
+    int atMost(vector<int>& nums, int goal){
+        int head, tail = 0, sum = 0, result = 0;
+        for (head = 0; head < nums.size(); head++) {
+            sum += nums[head];
+            while (sum > goal && tail <= head) {
+                sum -= nums[tail];
+                tail++;
+            }
+            result += head - tail + 1;
+        }
+        return result;
     }
 };
