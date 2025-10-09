3c4a6dc828291602e6a7e799f08180da861687b2
Prashik Patil <70844686+hotshott22@users.noreply.github.com>
2025-10-09 18:49:13 +0530

    Time: 12 ms (5.46%) | Memory: 29.3 MB (99.70%) - LeetSync

--- 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int n = nums.size();
-        int minSum = INT_MAX;
-        bool found = false;
-
-        for (int k = l; k <= r; ++k) {
-            int sum = 0;
-            for (int i = 0; i < n; ++i) {
-                sum += nums[i];
-                if (i >= k - 1) {
-                    if (sum > 0) {
-                        minSum = min(minSum, sum);
-                        found = true;
-                    }
-                    sum -= nums[i - k + 1];
-                }
+        int ans=INT_MAX,curr=0;;
+        for(int i=0;i<nums.size();++i)
+        {
+            curr=0;
+            for(int j=i;j<nums.size();++j)
+            {
+                curr+=nums[j];
+                if((j-i+1)>=l && (j-i+1)<=r && curr>0)
+                    ans=min(ans,curr);
             }
         }
-
-        return found ? minSum : -1;
-
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
     }
 };
