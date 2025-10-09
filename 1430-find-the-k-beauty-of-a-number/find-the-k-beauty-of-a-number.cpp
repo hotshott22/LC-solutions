326d951112271f3179e06ed87a2fa5d2e2f97737
Prashik Patil <70844686+hotshott22@users.noreply.github.com>
2025-10-09 11:42:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (9.47%) - LeetSync

---
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        const string s=to_string(num);
+
+        int ans=0;
+
+        for(int i=0;i+k<=s.length();i++)
+        {
+            int x=stoi(s.substr(i,k));
+            if(x!=0 && num%x==0)
+                ans++;
+        }
+        return ans;
+    }
+};
